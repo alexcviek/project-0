91dec565ffb163bf0bd6639920feeb2b61571e8e
Alex Cwiek <a.i.cwiek@gmail.com>
2017-05-19 13:31:04 +0100

    first 3 screens

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log('o hai, cheeseburger');
+$(() => {
+  function hideWindow1(){
+    $('.welcome').addClass('hidden');
+    $('.instructions').removeClass('hidden');
+  }
+  function hideWindow2(){
+    $('.instructions').addClass('hidden');
+    $('.welcome').removeClass('hidden');
+  }
+  function goToPlayScreen(){
+    $('.welcome').addClass('hidden');
+    $('.character').removeClass('hidden');
+  }
+  function toTheGame(){
+    $('.character').addClass('hidden');
+  }
+
+  $('#instructions').on('click', hideWindow1);
+  $('#back').on('click', hideWindow2);
+  $('#play').on('click', goToPlayScreen);
+  $('#start').on('click', toTheGame);
+
+});
